4719aee58cf810586bf76491c9f9a44ff38a2bf5
foundercode <foundercodetech@gmail.com>
2024-07-18 16:13:26 +0530

    test demmo

---
+++ lib/name.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class Name extends StatefulWidget {
+  const Name({super.key});
+
+  @override
+  State<Name> createState() => _NameState();
+}
+
+class _NameState extends State<Name> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("pink",),
+      ),
+      body: ListView(
+        children: [
+          Text("igigig"),
+          Text("fcytd6")
+        ],
+      ),
+    );
+  }
+}
